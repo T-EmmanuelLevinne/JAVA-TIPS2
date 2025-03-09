92bd5924c9a01e417b8a03273ab4f8804ef3c28d
T-EmmanuelLevinne <emmanuel.tecson@jmc.edu.ph>
2025-03-09 14:38:56 +0800

    Create ternary operator.java

---
+++ ternary operator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+ 
+public class Main {
+    public static void main(String[] args) {
+        
+        // ternary operator ? =  Return 1 of 2 values if a condition is true  
+        // variable = (condition) ? ifTrue : ifFalse;
+        
+        
+        
+      /* int score = 70;
+       
+       String passorfail = (score >= 60) ? "PASS" : "FAIL"; // if our score is lower than 60 that means we fail, but higher than 70 is pass
+       
+       System.out.print(passorfail);
+       
+       */
+       
+       /* 
+       int number = 3
+       
+       string evenorodd = (number % 2 ==0) ? "even" : "odd";
+       
+       System.out.print(evenorodd);
+       
+       
+    }
+}
